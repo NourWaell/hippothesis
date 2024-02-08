668f4d6519e296a05193f848e04604f37f820e41
nour <nourwael1530@gmail.com>
2024-02-08 22:43:02 +0200

    setup backend and landing page

---
+++ src/collections/Users.ts
@@ -0,0 +1,34 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+  slug: "users",
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>Hello, please verify your email.</p></br><a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify your email</a>`;
+      },
+    },
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      defaultValue: "user",
+      required: true,
+      type: "select",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "User",
+          value: "user",
+        },
+      ],
+    },
+  ],
+};
---
+++ src/components/NavItems.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, []);
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+        const isOpen = activeIndex === index;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default NavItems;
